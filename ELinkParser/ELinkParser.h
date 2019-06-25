e56872ccbbe961a9d60eade83853268372150987
zhenghongyi <xiaodaier@yeah.net>
2019-06-25 19:50:46 +0800

    Add basic link parser function

---
+++ ELinkParser/ELinkParser.h
@@ -0,0 +1,19 @@
+//
+//  ELinkParser.h
+//  ELinkParser
+//
+//  Created by Yi on 2019/6/25.
+//  Copyright © 2019 Yi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ELinkParser.
+FOUNDATION_EXPORT double ELinkParserVersionNumber;
+
+//! Project version string for ELinkParser.
+FOUNDATION_EXPORT const unsigned char ELinkParserVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ELinkParser/PublicHeader.h>
+#import "TFHpple.h"
+
